1b2c0ab61ee533b7b5abff8897e875abfea13b98
David Wenzel <dwenzel95@gmail.com>
2022-03-26 15:56:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+const form = () => document.querySelector("#create-task-form")
+  
+
+ // attach a submit event to the form
+ form().addEventListener("submit", handleSubmit)
+
+function handleSubmit(e){
+  e.preventDefault()
+  // create a new todo <li> and append it to the ul
+  const todoText = document.querySelector("#new-task-description").value
+  const li = document.createElement("li")
+  li.textContent = todoText
+  // next to the the next also append a button with an x
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = "X"
+  deleteButton.addEventListener("click", handleDelete)
+  li.append(deleteButton)
+  tasks.append(li)
+  // empty form
+  form().reset()
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
+
 });
